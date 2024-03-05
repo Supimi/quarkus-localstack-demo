3864969bd2d1cb607d64dee1f13187da00d335be
Supimi <supimi.15@cse.mrt.ac.lk>
2024-03-05 23:40:17 +0530

    update unit tests

--- src/test/java/com/dgsp/demo/UserResourceTest.java
+++ src/test/java/com/dgsp/demo/UserResourceTest.java
@@ -1,19 +1,24 @@
 package com.dgsp.demo;
 
 import com.dgsp.demo.dao.UserData;
+import com.dgsp.demo.dto.User;
+import com.dgsp.demo.dto.UserResponse;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.Key;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestProfile(ComponentTestProfile.class)
 @QuarkusTestResource(value = TestResource.class, restrictToAnnotatedClass = true)
@@ -26,6 +31,7 @@ class UserResourceTest {
     DynamoDbEnhancedClient dynamoDbEnhancedClient;
 
     DynamoDbTable<UserData> userDataTable;
+    ObjectMapper objectMapper = new ObjectMapper();
 
 
     @BeforeEach
@@ -36,15 +42,51 @@ class UserResourceTest {
 
 
     @Test
-    void testGetUsers() {
-        given()
+    void getUsersTest() {
+        UserResponse response = given()
                 .contentType("application/json")
                 .accept("application/json")
                 .when().get("/user")
                 .then()
                 .statusCode(200)
-                .body("success", is(true))
-                .log();
+                .extract()
+                .body()
+                .as(UserResponse.class);
+
+        assertTrue(response.success());
+        assertEquals(1, response.body().size());
+    }
+
+    @Test
+    void registerUserTest() throws JsonProcessingException {
+        User user = User.builder()
+                .userId("Jone")
+                .lastName("Doe")
+                .age(56)
+                .build();
+        UserResponse response = given()
+                .contentType("application/json")
+                .accept("application/json")
+                .body(objectMapper.writeValueAsBytes(user))
+                .when()
+                .post("/user")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(UserResponse.class);
+
+        assertTrue(response.success());
+        assertFalse(response.body().isEmpty());
+        assertNotNull(response.body().get(0).userId());
+        UserData saved = userDataTable.getItem(Key.builder()
+                .partitionValue(response.body().get(0).userId())
+                .build());
+
+        //assert with saved values
+        assertEquals(user.firstname(),saved.getFirstname());
+        assertEquals(user.lastName(),saved.getLastName());
+        assertEquals(user.age(),saved.getAge());
     }
 
     private void setupDB() {
@@ -80,7 +122,6 @@ class UserResourceTest {
         userDataTable.putItem(userData);
 
     }
-
 
 
 }
